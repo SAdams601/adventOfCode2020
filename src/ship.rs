bf7077612a2707b091c47c1d49fb7b847cd8b9e8
Stephen Adams <stephen.adams@ing.com>
2020-12-21 09:26:31 +0100

    Day 12.2

--- src/ship.rs
+++ src/ship.rs
@@ -1,6 +1,7 @@
 use crate::ship::Dir::*;
 use crate::ship::Instr::*;
 use std::cell::Cell;
+use std::f64::consts::PI;
 
 #[derive(Debug,Copy,Clone,PartialEq)]
 pub enum Dir {
@@ -38,10 +39,59 @@ impl Dir {
     } 
 }
 
+#[derive(Debug)]
+pub struct Waypoint {
+    x_offset : Cell<i32>,
+    y_offset : Cell<i32>
+}
+
+impl Waypoint {
+
+    fn new() -> Waypoint {
+        Waypoint { x_offset: Cell::new(10), y_offset: Cell::new(1) }
+    }
+
+    fn mv(&self, dir: Dir, amount: i32) {
+        let curr_y = self.y_offset.get();
+        let curr_x = self.x_offset.get();
+        match dir {
+            N => self.y_offset.set(curr_y + amount),
+            E => self.x_offset.set(curr_x + amount),
+            S => self.y_offset.set(curr_y - amount),
+            W => self.x_offset.set(curr_x - amount)
+        }
+    }
+
+    fn rotate(&self, way: i32, degrees: i32) {
+        let radians = -deg_to_rad(degrees) * way as f64;
+        let curr_x = self.x_offset.get();
+        let curr_y = self.y_offset.get();
+        let r : f64 = ((curr_x.pow(2) + curr_y.pow(2)) as f64).sqrt();
+        let mut phi = (curr_x as f64 /r).acos();
+        if curr_y < 0 {
+            phi *= -1.0;
+        }
+
+        phi += radians;
+        let new_x = (r * phi.cos()).round();
+        let new_y = (r * phi.sin()).round();
+        self.x_offset.set(new_x as i32);
+        self.y_offset.set((new_y) as i32);
+    }
+}
+
+fn deg_to_rad(degrees : i32) -> f64 {
+    (degrees as f64) * (PI/180.0)
+}
+
+fn rad_to_deg(radians : f64) -> i32 {
+    (radians * (180.0/PI)) as i32
+}
+
 pub struct Ship {
     x: Cell<i32>,
     y: Cell<i32>,
-    facing: Cell<Dir>
+    waypoint: Waypoint
 }
 
 #[derive(Debug)]
@@ -53,11 +103,11 @@ pub enum Instr {
 
 impl Ship {
     pub fn new() -> Ship {
-        Ship {x: Cell::new(0),y: Cell::new(0),facing: Cell::new(E)}
+        Ship {x: Cell::new(0),y: Cell::new(0),waypoint: Waypoint::new()}
     }
 
-    pub fn pos(&self) -> (i32, i32, Dir) {
-        (self.x.get(), self.y.get(), self.facing.get())
+    pub fn pos(&self) -> (i32, i32, i32, i32) {
+        (self.x.get(), self.y.get(), self.waypoint.x_offset.get(), self.waypoint.y_offset.get())
     }
 
     pub fn dist(&self) -> i32 {
@@ -75,32 +125,25 @@ impl Ship {
     }
 
     fn turn(&self, way : i32, degrees: i32) {
-        let curr_facing = self.facing.get();
-        let new_facing = curr_facing.turning(way, degrees);
-        println!("Turning from {:?}, to {:?}", curr_facing, new_facing);
-        self.facing.set(new_facing);
+        self.waypoint.rotate(way, degrees);
     }
 
-    fn sail_forward(&self, dist: i32) {
-        self.sail(self.facing.get(), dist);
+    fn sail_forward(&self, times: i32) {
+        let curr_x = self.x.get();
+        let curr_y = self.y.get();
+
+        self.x.set(curr_x + (self.waypoint.x_offset.get() * times));
+        self.y.set(curr_y + (self.waypoint.y_offset.get() * times));
     }
 
     fn sail(&self, dir: Dir, dist: i32) {
-        let curr_x = self.x.get();
-        let curr_y = self.y.get();
-        match dir {
-            N => self.y.set(curr_y + dist),
-            E => self.x.set(curr_x + dist),
-            S => self.y.set(curr_y - dist),
-            W => self.x.set(curr_x - dist),
-        }
+        self.waypoint.mv(dir, dist);
     }
 }
 
 pub fn parse(line: &str) -> Instr {
     let instr_type = line.chars().take(1).collect::<Vec<char>>()[0];
     let num: i32 = line[1..].parse().expect("Could not parse number");
-    println!("Parsing {:?} with num {:?}", instr_type, num);
     match instr_type {
         'F' => Forward(num),
         'L' => Turn(-1,num),
